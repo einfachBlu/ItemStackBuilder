67a0f4f384773de9f5c05ae9900892fc33425df3
Bluplayz <bluplayz98@gmail.com>
2021-06-23 21:33:01 +0200

    removed local repository

--- src/main/java/de/blu/itemstackbuilder/ItemStackBuilderPlugin.java
+++ src/main/java/de/blu/itemstackbuilder/ItemStackBuilderPlugin.java
@@ -4,6 +4,7 @@ import de.blu.itemstackbuilder.builder.ItemStackBuilder;
 import de.blu.itemstackbuilder.builder.LeatherArmorItemStackBuilder;
 import de.blu.itemstackbuilder.enchantment.GlowEnchantment;
 import lombok.Getter;
+import net.md_5.bungee.api.ChatMessageType;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Material;
@@ -35,91 +36,7 @@ public final class ItemStackBuilderPlugin extends JavaPlugin {
                 CraftPlayer craftPlayer = (CraftPlayer) player;
 
                 player.sendMessage("locale: " + craftPlayer.getLocale());
-                player.sendMessage("clientBrandName: " + craftPlayer.getClientBrandName());
-
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.skull()
-                            .profile(craftPlayer.getProfile())
-                            .displayName("§c§lDas ist ein TestName")
-                            .lore("§7Line1", "§7Line2", "§7Line3", "", "§aLine5", "")
-                            .glow()
-                            .build());
-
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.air()
-                            .type(Material.DIRT)
-                            .displayName("§c§lDas ist ein glow Item")
-                            .clearLore()
-                            .storeNBTInt("id", 5)
-                            .storeNBTInt("id2", 10)
-                            .storeNBTBoolean("hasGlow", true)
-                            .glow()
-                            .build());
-
-                player
-                    .getInventory()
-                    .addItem(ItemStackBuilder.book(true)
-                            .title("§aTitel 1")
-                            .author("§ceinfachBlu")
-                            .addPage("Test")
-                            .addPage("Test2")
-                            .addPage("Test3")
-                            .addPage("§aTe§cst§b4")
-                            .displayName("§c§lBeschreibbar")
-                            .build());
-
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.book(false)
-                                .title("§aTitel 1")
-                                .author("§ceinfachBlu")
-                                .addPage("Test")
-                                .addPage("Test2")
-                                .addPage("Test3")
-                                .addPage("§aTe§cst§b4")
-                                .displayName("§c§lnicht Beschreibbar")
-                                .build());
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.potion()
-                            .effect(PotionType.SPEED, 2, true)
-                            .displayName("§c§lPotion 1")
-                            .build());
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.potion()
-                            .effect(PotionType.SPEED, 1, false)
-                            .displayName("§c§lPotion 2")
-                            .build());
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.potion()
-                            .effect(PotionType.SPEED, 2, true)
-                            .displayName("§c§lPotion 3")
-                            .build());
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.potion()
-                            .effect(PotionType.SPEED, 1, false)
-                            .displayName("§c§lPotion 4")
-                            .build());
-                player
-                    .getInventory()
-                    .addItem(
-                        ItemStackBuilder.armor(LeatherArmorItemStackBuilder.ArmorElement.CHESTPLATE)
-                            .dye(Color.ORANGE)
-                            .displayName("§c§lOrange Jacke")
-                            .glow()
-                            .build());
+                player.sendMessage(ChatMessageType.ACTION_BAR);
               }
             },
             this);
